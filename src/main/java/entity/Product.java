97ab5a6d05b7a5e524442fe99345e9dba4199b6d
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-23 20:52:15 -0300

    feat: Product

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,37 @@
+package entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product extends PanacheEntity {
+    private String nome;
+    private Long stock;
+    private Double price;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getStock() {
+        return stock;
+    }
+
+    public void setStock(Long stock) {
+        this.stock = stock;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
