d4b98e9244208d177e0c75af4bc9c16d23e7ca2e
Marcelo Santos <rocha.mer21@gmail.com>
2023-10-23 23:39:55 -0300

    feat: add ProductService

--- src/main/java/tech/marcelorsantos/service/ProductService.java
+++ src/main/java/tech/marcelorsantos/service/ProductService.java
@@ -1,6 +1,8 @@
 package tech.marcelorsantos.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
+import tech.marcelorsantos.dto.ProductDTO;
 import tech.marcelorsantos.entity.Product;
 
 import java.util.List;
@@ -9,5 +11,17 @@ import java.util.List;
 public class ProductService {
     public List<Product> productList(){
         return Product.listAll();
+    }
+
+    @Transactional
+    public Product saveProduct(ProductDTO dto){
+        Product product = new Product();
+
+        product.setNome(dto.getNome());
+        product.setStock(dto.getStock());
+        product.setPrice(dto.getPrice());
+        product.persist();
+
+        return product;
     }
 }
